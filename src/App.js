8f0650b02cd5f9a602fff715f1c126eb2064bf49
jamesncox <jamesncox@yahoo.com>
2021-02-11 12:44:16 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          learn react
+          learn react NOW
         </a>
       </header>
     </div>
