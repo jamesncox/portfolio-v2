58929101df13e4a73140cc2240f0e3885ea2679b
jamesncox <jamesncox@yahoo.com>
2021-02-14 13:51:10 -0600

    Add .main-container to App.css and import to App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import logo from './logo.svg';
-import './App.css';
+import './stylesheets/App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          learn react like a boss
-        </a>
-      </header>
+    <div className="main-container">
+      <div className="box a">A</div>
+      <div className="box b">B</div>
+      <div className="box c">C</div>
+      <div className="box D">D</div>
+      <div className="box E">E</div>
+      <div className="box F">F</div>
     </div>
   );
 }
