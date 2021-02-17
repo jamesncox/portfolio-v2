67920d61dc5b9952999418ad164223c6588b0028
jamesncox <jamesncox@yahoo.com>
2021-02-17 17:56:37 -0600

    Update NavBar with react-router Link

--- src/components/layout/NavBar.js
+++ src/components/layout/NavBar.js
@@ -1,13 +1,14 @@
 import React from 'react'
+import {Link} from 'react-scroll'
 
 function NavBar() {
 
     return (
         <nav className="menu-wrapper">
-            <button className="menu-buttons">About</button>
-            <button className="menu-buttons">Portfolio</button>
-            <button className="menu-buttons">Blog</button>
-            <button className="menu-buttons">Contact</button>
+            <button className="menu-buttons"><Link activeClass="active" to="about" spy={true} smooth={true}>About</Link></button>
+            <button className="menu-buttons"><Link activeClass="active" to="projects" spy={true} smooth={true}>Projects</Link></button>
+            <button className="menu-buttons"><Link activeClass="active" to="blog" spy={true} smooth={true}>Blog</Link></button>
+            <button className="menu-buttons"><Link activeClass="active" to="contact" spy={true} smooth={true}>Contact</Link></button>
         </nav>
     )
 }
