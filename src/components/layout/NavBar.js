97246bcf319af2126e7070877add89df8746df7b
jamesncox <jamesncox@yahoo.com>
2021-02-17 16:02:14 -0600

    Remove react-router-dom

--- src/components/layout/NavBar.js
+++ src/components/layout/NavBar.js
@@ -1,15 +1,10 @@
 import React from 'react'
-// import { Link as RouterLink } from 'react-router-dom';
-
 
 function NavBar() {
 
     return (
         <nav className="menu-wrapper">
-            {/* <RouterLink to="/"> */}
-                <button className="menu-buttons">About</button>
-            {/* </RouterLink> */}
-
+            <button className="menu-buttons">About</button>
             <button className="menu-buttons">Portfolio</button>
             <button className="menu-buttons">Blog</button>
             <button className="menu-buttons">Contact</button>
