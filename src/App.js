6bf4ad3ec083d4075482271b8eaa6dc43239ee48
jamesncox <jamesncox@yahoo.com>
2021-02-17 10:03:45 -0600

    Create components/layout/Navbar.js and import to App.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,12 @@
-import logo from './logo.svg';
-import './stylesheets/App.css';
+import React from 'react'
+import './stylesheets/App.css'
+
+import NavBar from './components/layout/NavBar'
 
 function App() {
   return (
     <div className="main-container">
-      <div className="box a">A</div>
-      <div className="box b">B</div>
-      <div className="box c">C</div>
-      <div className="box D">D</div>
-      <div className="box E">E</div>
-      <div className="box F">F</div>
-      <div className="box a">A</div>
-      <div className="box b">B</div>
-      <div className="box c">C</div>
-      <div className="box D">D</div>
-      <div className="box E">E</div>
-      <div className="box F">F</div>
-      <div className="box a">A</div>
-      <div className="box b">B</div>
-      <div className="box c">C</div>
-      <div className="box D">D</div>
-      <div className="box E">E</div>
-      <div className="box F">F</div>
+      <NavBar />
     </div>
   );
 }
---
+++ src/components/layout/NavBar.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function NavBar() {
+
+    return (
+        <h1>Hi there</h1>
+    )
+}
+
+export default NavBar
