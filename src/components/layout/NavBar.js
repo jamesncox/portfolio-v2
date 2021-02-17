dde7103bcb25bb7b95c89a1b9ce7df9bc44a06ef
jamesncox <jamesncox@yahoo.com>
2021-02-17 14:51:37 -0600

    Change NavBar.js <a> to <button> / Update App.css

--- src/components/layout/NavBar.js
+++ src/components/layout/NavBar.js
@@ -3,11 +3,11 @@ import React from 'react'
 function NavBar() {
 
     return (
-        <nav className="menu-links">
-            <a>About</a>
-            <a>Portfolio</a>
-            <a>Blog</a>
-            <a>Contact</a>
+        <nav className="menu-wrapper">
+            <button className="menu-buttons">About</button>
+            <button className="menu-buttons">Portfolio</button>
+            <button className="menu-buttons">Blog</button>
+            <button className="menu-buttons">Contact</button>
         </nav>
     )
 }
