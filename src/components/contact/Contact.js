864fe1f61eede28efec27ff725cc50ada4231d24
jamesncox <jamesncox@yahoo.com>
2021-02-18 12:49:35 -0600

    Remove section titles from About, Blog, Contact and Projects.js

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -4,7 +4,6 @@ function Contact() {
 
     return (
         <div id="contact" className="practice-scroll">
-          <h1 className="selected-section">Contact</h1>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, repellendus. Totam nihil similique a repellat minus dolor amet quasi. Corporis nulla quaerat iste, sed quasi ab dolorem maxime minima animi.
         </div>
     )
