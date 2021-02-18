9d5de4a654490e7516de6538997f2563315d3999
jamesncox <jamesncox@yahoo.com>
2021-02-17 18:14:17 -0600

    Create Blog.js and import to App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './stylesheets/Responsive.css'
 import './stylesheets/NavMenu.css'
 import About from './components/about/About'
 import Projects from './components/projects/Projects'
+import Blog from './components/blog/Blog'
 
 import Header from './components/layout/Header'
 
@@ -13,6 +14,7 @@ function App() {
       <Header />
       <About />
       <Projects />
+      <Blog />
     </div>
   );
 }
