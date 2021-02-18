d240cb0cd5b2267d6e7af637d806bec3adc40273
jamesncox <jamesncox@yahoo.com>
2021-02-17 18:05:41 -0600

    Update App.css with .practice-scroll

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -3,7 +3,7 @@ import React from 'react'
 function About() {
 
     return (
-        <div id="about" style={{height: 1000}}>
+        <div id="about" className="practice-scroll">
           <h1>This is About section</h1>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, repellendus. Totam nihil similique a repellat minus dolor amet quasi. Corporis nulla quaerat iste, sed quasi ab dolorem maxime minima animi.
         </div>
--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Projects() {
 
     return (
-        <div id="projects" style={{height: 1000}}>
+        <div id="projects" className="practice-scroll">
           <h1>This is Projects section</h1>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, repellendus. Totam nihil similique a repellat minus dolor amet quasi. Corporis nulla quaerat iste, sed quasi ab dolorem maxime minima animi.
         </div>
