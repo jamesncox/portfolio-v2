068edd83f7c301e263ad6175272abe3b83e1e454
jamesncox <jamesncox@yahoo.com>
2021-02-18 17:32:33 -0600

    Update ShortBio and import to About.js

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import BioSelect from './BioSelect'
+import ShortBio from './ShortBio'
 
 function About() {
 
     return (
+      <>
         <BioSelect />
+        <ShortBio />
+      </>
     )
 }
 
--- src/components/about/ShortBio.js
+++ src/components/about/ShortBio.js
@@ -2,6 +2,9 @@ import React from 'react'
 
 function ShortBio() {
 
+    return (
+        <p>I code things.</p>
+    )
 }
 
 export default ShortBio
