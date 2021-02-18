5dfa86de41a075ed471e893e01fafc66db8f721e
jamesncox <jamesncox@yahoo.com>
2021-02-18 09:29:41 -0600

    Update App.css margins

--- src/components/layout/NavBar.js
+++ src/components/layout/NavBar.js
@@ -5,10 +5,10 @@ function NavBar() {
 
     return (
         <nav className="menu-wrapper">
-            <button className="menu-buttons"><Link activeClass="active" to="about" spy={true} smooth={true} offset={-100}>About</Link></button>
-            <button className="menu-buttons"><Link activeClass="active" to="projects" spy={true} smooth={true} offset={-100}>Projects</Link></button>
-            <button className="menu-buttons"><Link activeClass="active" to="blog" spy={true} smooth={true} offset={-100}>Blog</Link></button>
-            <button className="menu-buttons"><Link activeClass="active" to="contact" spy={true} smooth={true} offset={-100}>Contact</Link></button>
+            <button className="menu-buttons"><Link activeClass="active" to="about" spy={true} smooth={true} offset={-100} duration={400}>About</Link></button>
+            <button className="menu-buttons"><Link activeClass="active" to="projects" spy={true} smooth={true} offset={-100} duration={400}>Projects</Link></button>
+            <button className="menu-buttons"><Link activeClass="active" to="blog" spy={true} smooth={true} offset={-100} duration={400}>Blog</Link></button>
+            <button className="menu-buttons"><Link activeClass="active" to="contact" spy={true} smooth={true} offset={-100} duration={400}>Contact</Link></button>
         </nav>
     )
 }
