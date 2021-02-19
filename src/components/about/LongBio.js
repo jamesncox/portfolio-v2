666e62fc2ba3d9eeb98a813ba11f1e0ad8a55865
jamesncox <jamesncox@yahoo.com>
2021-02-19 12:49:43 -0600

    Update App.css and NavMenu.css fontsizes by 15% bigger

--- src/components/about/LongBio.js
+++ src/components/about/LongBio.js
@@ -4,7 +4,7 @@ export default function LongBio() {
 
     return (
         <>
-            <p className="text">!! UNDER CONSTRUCTION !!</p>
+            <p className="text">⚠️ UNDER CONSTRUCTION ⚠️</p>
             <p className="text">Really long bio coming soon.</p>
             <p className="text">It will be worth the wait, I promise.</p>
             <p className="text">You might even laugh a little. Or cry.</p>
--- src/components/about/StalkerBio.js
+++ src/components/about/StalkerBio.js
@@ -4,7 +4,7 @@ export default function StalkerBio() {
 
     return (
         <>
-            <p className="text">!! UNDER CONSTRUCTION !!</p>
+            <p className="text">⚠️ UNDER CONSTRUCTION ⚠️</p>
             <p className="text">Are you stalking me? Do you want to know everything this is to know about me?</p>
             <p className="text">Well you've come to right place.</p>
             <p className="text">Very soon I'll have a lengthy bio up.</p>
