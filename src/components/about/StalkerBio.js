619f9588c7236279d72b1f5c7521eb7c36db5f79
jamesncox <jamesncox@yahoo.com>
2021-02-19 15:21:52 -0600

    Update StalkerBio.js

--- src/components/about/StalkerBio.js
+++ src/components/about/StalkerBio.js
@@ -8,7 +8,7 @@ export default function StalkerBio() {
             <p className="text">Are you stalking me? Do you want to know everything this is to know about me?</p>
             <p className="text">Well you've come to right place.</p>
             <p className="text">Very soon I'll have a lengthy bio up.</p>
-            <p className="text">You might even regret read this much about me.</p>
+            <p className="text">You might even regret ever reading this much about me.</p>
         </>
  )
 }
