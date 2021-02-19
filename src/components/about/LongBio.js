20426d47827eb91181c9ddd378d6be0d9a187353
jamesncox <jamesncox@yahoo.com>
2021-02-19 14:58:45 -0600

    Update LongBio with <p> text and ImageLoad gifs

--- src/components/about/LongBio.js
+++ src/components/about/LongBio.js
@@ -1,13 +1,45 @@
 import React from 'react'
+import ImageLoad from '../../hooks/ImageLoad'
+import Maze from '../../assets/images/maze.gif'
+import MazeTiny from '../../assets/images/maze-tiny.jpg'
+import Sleeves from '../../assets/images/sleeves.gif'
+import SleevesTiny from '../../assets/images/sleeves-tiny.jpg'
+import Dancing from '../../assets/images/dancing.gif'
+import DancingTiny from '../../assets/images/dancing-tiny.jpg'
 
 export default function LongBio() {
 
     return (
         <>
-            <p className="text">⚠️ UNDER CONSTRUCTION ⚠️</p>
-            <p className="text">Really long bio coming soon.</p>
-            <p className="text">It will be worth the wait, I promise.</p>
-            <p className="text">You might even laugh a little. Or cry.</p>
+            <p className="text">My journey to becoming a software developer has been long and winding.</p>
+            <div className="image-wrapper">
+                <ImageLoad
+                    className="gif"
+                    style={{justifySelf: "start"}}
+                    src={Maze}
+                    placeholder={MazeTiny}
+                    alt="Gif of balls navigating a maze"
+                />
+            </div>
+            <p className="text">I am transitioning from a restaurant background. From putting out literal fires 🔥 to squashing bugs 🐛, I am used to rolling up my sleeves, and doing the dirty work.</p>
+            <div className="image-wrapper">
+                <ImageLoad
+                    className="gif"
+                    src={Sleeves}
+                    placeholder={SleevesTiny}
+                    alt="Gif of a man rolling up his sleeves and then rubbing his hands together."
+                />
+            </div>
+            <p className="text">But I digress.</p>
+            <p className="text">I learned to code because I want to satisfy my creative urges, provide for my family, and when the time is right, build a log home in the mountains and work from our wooded retreat.</p>
+            <div className="image-wrapper">
+                <ImageLoad
+                    className="gif"
+                    src={Dancing}
+                    placeholder={DancingTiny}
+                    alt="Gif of a man on the same hills from The Sound of Music dancing in a similar fashion as Julie Andrews"
+                />
+            </div>
         </>
  )
 }
