edd141610a86b54ade0182fcf1b4b49a139c55bd
jamesncox <jamesncox@yahoo.com>
2021-02-20 15:27:41 -0600

    Add Do Did Done to FeaturedProjects.js

--- src/components/projects/FeaturedProjects.js
+++ src/components/projects/FeaturedProjects.js
@@ -4,6 +4,8 @@ import TheBoredinator from '../../assets/images/the-boredinator.jpg'
 import TheBoredinatorTiny from '../../assets/images/the-boredinator-tiny.jpg'
 import GifFit from '../../assets/images/GIF-FIT.jpg'
 import GifFitTiny from '../../assets/images/GIF-FIT-tiny.jpg'
+import DoDidDone from '../../assets/images/do-did-done.jpg'
+import DoDidDoneTiny from '../../assets/images/do-did-done-tiny.jpg'
 
 export default function FeaturedProjects() {
 
@@ -38,16 +40,16 @@ export default function FeaturedProjects() {
                 </div>
 
                 <div className="projects-card">
-                    <p className="project-title">The Boredinator</p>
-                    <p className="project-subtitle">Activities and inspiration to stave off boredom</p>
+                    <p className="project-title">Do Did Done</p>
+                    <p className="project-subtitle">Keep track of your todos by category</p>
                         <ImageLoad
                             className="project-image"
-                            src={TheBoredinator}
-                            placeholder={TheBoredinatorTiny}
+                            src={DoDidDone}
+                            placeholder={DoDidDoneTiny}
                             alt="Screenshot of The Boredinator home page"
                         />
                     <p className="project-description">Featuring: </p>
-                    <p className="project-description">Light/Dark theme toggle with localStorage to save user's preference on page reload; CSS variables for efficient light and dark theme management; State management with Redux; CSS-only design, no CSS libraries; Custom, reusable Hook called ImageLoad to lazy load images; Custom JSON data objects, simulating a local API; Separate and reusable components</p>
+                    <p className="project-description">React frontend and a Rails API backend. The React frontend features Material UI components, React router, Redux and local state management, functional components and React hooks and a thoughtful design for improved UI and UX. The frontend consumes the Rails API with full CRUD functionality. The Rails API backend is hosted on Heroku and features a PostgreSQL database. It handles sessions, cookies, CRUD functionality, separation of concerns and MVC structure.</p>
                 </div>
             </div>
         </>
