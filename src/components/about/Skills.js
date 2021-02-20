4d44ea7ea7e55fb5230c2fceec6b17bed9a46ea2
jamesncox <jamesncox@yahoo.com>
2021-02-20 09:51:47 -0600

    Create Skills.js

---
+++ src/components/about/Skills.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Skills() {
+
+    return (
+        <>
+            <p style={{marginTop: "3rem"}} className="section-title">work</p>
+        </>
+    )
+}
