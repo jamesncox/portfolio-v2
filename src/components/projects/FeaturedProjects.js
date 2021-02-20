cccf99c181dcfacee7a3b322858ca089d0aea89c
jamesncox <jamesncox@yahoo.com>
2021-02-20 14:35:14 -0600

    Replace the-boredinator.jpg with better cropped one

--- src/components/projects/FeaturedProjects.js
+++ src/components/projects/FeaturedProjects.js
@@ -9,7 +9,7 @@ export default function FeaturedProjects() {
         <>
             <p className="section-title">featured projects</p>
             <div className="projects-wrapper">
-                {/* <div className="projects-card"> */}
+                <div className="projects-card">
                     <p className="project-title">The Boredinator</p>
                     <p className="project-subtitle">Activities and inspiration to stave off boredom</p>
                         <ImageLoad
@@ -19,7 +19,7 @@ export default function FeaturedProjects() {
                             alt="Screenshot of The Boredinator home page"
                         />
                     <p className="project-description">Featuring: Light/Dark theme toggle with localStorage to save user's preference on page reload; CSS variables for efficient light and dark theme management; State management with Redux; CSS-only design, no CSS libraries; Custom, reusable Hook called ImageLoad to lazy load images; Custom JSON data objects, simulating a local API; Separate and reusable components</p>
-                {/* </div> */}
+                </div>
             </div>
         </>
     )
