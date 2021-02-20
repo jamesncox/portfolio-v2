9f24056364be4dd8e1fc7f3058ffddbd7bb57ab1
jamesncox <jamesncox@yahoo.com>
2021-02-20 11:13:54 -0600

    Update Blog, Contact and Projects with placeholder titles

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -3,9 +3,12 @@ import React from 'react'
 function Contact() {
 
     return (
-        <div id="contact" className="practice-scroll">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, repellendus. Totam nihil similique a repellat minus dolor amet quasi. Corporis nulla quaerat iste, sed quasi ab dolorem maxime minima animi.
-        </div>
+      <div  id="contact">
+      <p className="section-title">CONTACT SECTION</p>
+      <div className="practice-scroll">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, repellendus. Totam nihil similique a repellat minus dolor amet quasi. Corporis nulla quaerat iste, sed quasi ab dolorem maxime minima animi.
+      </div>
+    </div>
     )
 }
 
--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -3,9 +3,12 @@ import React from 'react'
 function Projects() {
 
     return (
-        <div id="projects" className="practice-scroll">
+      <div  id="projects">
+        <p className="section-title">PROJECTS SECTION</p>
+        <div className="practice-scroll">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, repellendus. Totam nihil similique a repellat minus dolor amet quasi. Corporis nulla quaerat iste, sed quasi ab dolorem maxime minima animi.
         </div>
+      </div>
     )
 }
 
