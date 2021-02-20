38baaf20ae9265ab1ba6ed29e062b5442886c734
jamesncox <jamesncox@yahoo.com>
2021-02-20 13:42:43 -0600

    Add The Boredinator image to FeaturedProjects.js

--- src/components/projects/FeaturedProjects.js
+++ src/components/projects/FeaturedProjects.js
@@ -1,12 +1,20 @@
 import React from 'react'
+import ImageLoad from '../../hooks/ImageLoad'
+import TheBoredinator from '../../assets/images/the-boredinator.jpg'
+import TheBoredinatorTiny from '../../assets/images/the-boredinator-tiny.jpg'
 
 export default function FeaturedProjects() {
 
     return (
         <>
             <p className="section-title">featured projects</p>
-            <div className="practice-scroll">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, repellendus. Totam nihil similique a repellat minus dolor amet quasi. Corporis nulla quaerat iste, sed quasi ab dolorem maxime minima animi.
+            <div className="image-wrapper">
+                <ImageLoad
+                    className="image"
+                    src={TheBoredinator}
+                    placeholder={TheBoredinatorTiny}
+                    alt="Screenshot of The Boredinator home page"
+                />
             </div>
         </>
     )
