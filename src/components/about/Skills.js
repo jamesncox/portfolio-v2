b2b11aedc17f7c6c56085b24544c217a506373d0
jamesncox <jamesncox@yahoo.com>
2021-02-20 10:28:12 -0600

    Create .skills-grid and .skills, .include in Skills.js

--- src/components/about/Skills.js
+++ src/components/about/Skills.js
@@ -4,7 +4,20 @@ export default function Skills() {
 
     return (
         <>
-            <p className="section-title" style={{height: "800px"}} >skills</p>
+            <p className="section-title">skills</p>
+            <div className="skill-grid">
+                <div className="skills">1</div>
+                <div className="skills">2</div>
+                <div className="skills">3</div>
+                <div className="skills">4</div>
+                <div className="skills">5</div>
+                <div className="skills">6</div>
+                <div className="skills">7</div>
+                <div className="skills">8</div>
+                <div className="skills">9</div>
+                <div className="skills">10</div>
+                <div style={{height: "800px"}}></div>
+            </div>
         </>
     )
 }
