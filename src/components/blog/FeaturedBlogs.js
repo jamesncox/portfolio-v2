abadb8d46354c6a999691ea17fb689ed2fea1e4b
jamesncox <jamesncox@yahoo.com>
2021-02-22 13:28:41 -0600

    Add useRef() blog post to FeaturedBlogs.js

--- src/components/blog/FeaturedBlogs.js
+++ src/components/blog/FeaturedBlogs.js
@@ -4,6 +4,8 @@ import UseState from '../../assets/images/useState.jpg'
 import UseStateTiny from '../../assets/images/useStateTiny.jpg'
 import UseEffect from '../../assets/images/useEffect.jpg'
 import UseEffectTiny from '../../assets/images/useEffectTiny.jpg'
+import UseRef from '../../assets/images/useRef.jpg'
+import UseRefTiny from '../../assets/images/useRefTiny.jpg'
 
 export default function FeaturedBlogs() {
     return (
@@ -25,9 +27,20 @@ export default function FeaturedBlogs() {
                     title="React Hooks Series: useEffect"
                     src={UseEffect}
                     placeholder={UseEffectTiny}
-                    alt="Black background with the word useEffect written in bold orange"
+                    alt="Black background with the word useEffect written in bold blue"
                     description="Part two in my React Hooks Series takes a look at the useEffect hook and how I implememnt in a small timer app I created in Codesandbox."
                     url="https://dev.to/jamesncox/react-hook-series-useeffect-in2"
+                />
+            </div>
+
+            <div className="projects-wrapper">
+                <DisplayBlog
+                    title="React Hooks Series: useRef"
+                    src={UseRef}
+                    placeholder={UseRefTiny}
+                    alt="Black background with the word useRef written in bold orange"
+                    description="Part three in the React Hooks Series talks about my personal favorite hook: useRef. We look at how useRef solves a problem with pausing and starting the timer in my Codesandbox example."
+                    url="https://dev.to/jamesncox/react-hooks-series-useref-27mk"
                 />
             </div>
         </>
