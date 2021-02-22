3c6210b1d7f0f220c4b6d485dd13b404adedf483
jamesncox <jamesncox@yahoo.com>
2021-02-22 12:58:28 -0600

    Create .blog-title and add to DisplayBlog.js

--- src/components/blog/Blog.js
+++ src/components/blog/Blog.js
@@ -1,14 +1,12 @@
 import React from 'react'
+import FeaturedBlogs from './FeaturedBlogs'
 
 function Blog() {
 
     return (
       <div  id="blog">
-      <p className="section-title">BLOG SECTION</p>
-      <div className="practice-scroll">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, repellendus. Totam nihil similique a repellat minus dolor amet quasi. Corporis nulla quaerat iste, sed quasi ab dolorem maxime minima animi.
+        <FeaturedBlogs />
       </div>
-    </div>
     )
 }
 
--- src/components/blog/FeaturedBlogs.js
+++ src/components/blog/FeaturedBlogs.js
@@ -9,13 +9,12 @@ export default function FeaturedBlogs() {
             <p className="section-title">featured blog posts</p>
             <div className="projects-wrapper">
                 <DisplayBlog
-                    title="The Boredinator"
+                    title="React Hooks Series: useState"
                     // subtitle="Activities and inspiration to stave off boredom"
                     src={UseState}
                     placeholder={UseStateTiny}
-                    // id="collapsible-boredinator"
                     alt="Black background with the word useState written in bold and white"
-                    // features={BoredinatorFeatures}
+                    description="Part one in my React Hooks Series. I examine the useState hook in a basic timer app with examples from Codesandbox."
                     url="https://dev.to/jamesncox/react-hooks-series-usestate-12ne"
                 />
             </div>
