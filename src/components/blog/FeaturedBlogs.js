b526421335264d9c29ec0ff1f9b56156f419166a
jamesncox <jamesncox@yahoo.com>
2021-02-22 12:46:53 -0600

    Stub out FeaturedBlogs with DisplayBlog hook

--- src/components/blog/FeaturedBlogs.js
+++ src/components/blog/FeaturedBlogs.js
@@ -1,7 +1,24 @@
-// import React from 'react'
+import React from 'react'
+import DisplayBlog from '../../hooks/DisplayBlog'
+import UseState from '../../assets/images/useState.jpg'
+import UseStateTiny from '../../assets/images/useStateTiny.jpg'
 
-// export default function FeaturedBlogs() {
-//     return (
-
-//     )
-// }
+export default function FeaturedBlogs() {
+    return (
+        <>
+            <p className="section-title">featured blog posts</p>
+            <div className="projects-wrapper">
+                <DisplayBlog
+                    title="The Boredinator"
+                    // subtitle="Activities and inspiration to stave off boredom"
+                    src={UseState}
+                    placeholder={UseStateTiny}
+                    // id="collapsible-boredinator"
+                    alt="Black background with the word useState written in bold and white"
+                    // features={BoredinatorFeatures}
+                    url="https://dev.to/jamesncox/react-hooks-series-usestate-12ne"
+                />
+            </div>
+        </>
+    )
+}
