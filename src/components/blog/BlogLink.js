9d7b18bdb67d4c45b2885266cd0e27fe610413f3
jamesncox <jamesncox@yahoo.com>
2021-02-23 11:10:09 -0600

    Update DisplayBlog icon placement and App.css

--- src/components/blog/BlogLink.js
+++ src/components/blog/BlogLink.js
@@ -5,7 +5,7 @@ export default function BlogLink() {
     return (
         <a className="see-more-blogs" href="https://dev.to/jamesncox" rel="noopener noreferrer" target="_blank">
             <p className="text">SEE MORE POSTS</p>
-            <div className="arrow-icon">
+            {/* <div className="arrow-icon">
                 <svg 
                     className="icon-link"
                     xmlns="http://www.w3.org/2000/svg" 
@@ -13,7 +13,7 @@ export default function BlogLink() {
                 >
                     <path fillRule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/>
                 </svg>
-            </div>
+            </div> */}
         </a>
     )
 }
--- src/hooks/DisplayBlog.js
+++ src/hooks/DisplayBlog.js
@@ -5,18 +5,10 @@ const DisplayBlog = React.memo(({ title, src, placeholder, alt, description, url
 
     return (
       <div className="blog-card">
+        <div className="blog-icon-wrapper">
         <p className="blog-title">{title}</p>
-        <ImageLoad
-          className="project-image"
-          src={src}
-          placeholder={placeholder}
-          alt={alt}
-        />
-        <p className="project-description">{description}</p>
-        <div className="project-icon-wrapper">
-          {/* <p className="icon-description">visit</p> */}
 
-          {/* <div className="arrow-icon">
+          <div className="arrow-icon">
             <a href={url} rel="noopener noreferrer" target="_blank">
               <svg 
                   className="icon-link"
@@ -26,10 +18,10 @@ const DisplayBlog = React.memo(({ title, src, placeholder, alt, description, url
                   <path fillRule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/>
               </svg>
             </a>
-          </div> */}
+          </div>
 
 
-            <div className="icons">
+            {/* <div className="icons">
               <a href={url} rel="noopener noreferrer" target="_blank">
                   <svg
                     className="icon-link"
@@ -39,8 +31,16 @@ const DisplayBlog = React.memo(({ title, src, placeholder, alt, description, url
                     <path d="M14 0a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h12zM5.904 10.803L10 6.707v2.768a.5.5 0 0 0 1 0V5.5a.5.5 0 0 0-.5-.5H6.525a.5.5 0 1 0 0 1h2.768l-4.096 4.096a.5.5 0 0 0 .707.707z" />
                   </svg>
               </a>
-            </div>
+            </div> */}
+            
         </div>
+        <ImageLoad
+          className="project-image"
+          src={src}
+          placeholder={placeholder}
+          alt={alt}
+        />
+        <p className="project-description">{description}</p>
       </div>
     );
 })
