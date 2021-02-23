d31471625f788f17c1723b6b93202a0a2fc4c08b
jamesncox <jamesncox@yahoo.com>
2021-02-23 10:21:45 -0600

    Add Resume.pdf to assets/pdfs

--- src/components/contact/Resume.js
+++ src/components/contact/Resume.js
@@ -3,6 +3,11 @@ import React from 'react'
 export default function Resume() {
 
     return (
-        <p className="section-title">resume</p>
+        <>
+            <p className="section-title">resume</p>
+            <a className="email" href="https://gitconnected.com/jamesncox/resume" rel="noopener noreferrer" target="_blank">
+                <p>@gitconnected</p>
+            </a>
+        </>
     )
 }
