b88e17f582976116b8fa19b9a964bfca55ffd392
jamesncox <jamesncox@yahoo.com>
2021-02-23 09:39:39 -0600

    Update Contact.js

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -3,11 +3,9 @@ import React from 'react'
 function Contact() {
 
     return (
-      <div  id="contact">
-      <p className="section-title">CONTACT SECTION</p>
-      <div className="practice-scroll">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, repellendus. Totam nihil similique a repellat minus dolor amet quasi. Corporis nulla quaerat iste, sed quasi ab dolorem maxime minima animi.
-      </div>
+      <div  id="contact" style={{height: "1000px"}}>
+      <p className="section-title">contact</p>
+      
     </div>
     )
 }
