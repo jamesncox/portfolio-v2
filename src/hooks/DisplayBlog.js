6d1dd60982127227758a38cbe27774c1acc058cc
jamesncox <jamesncox@yahoo.com>
2021-02-22 21:54:29 -0600

    Stub out different icon for DisplayBlog.js

--- src/hooks/DisplayBlog.js
+++ src/hooks/DisplayBlog.js
@@ -15,6 +15,20 @@ const DisplayBlog = React.memo(({ title, src, placeholder, alt, description, url
         <p className="project-description">{description}</p>
         <div className="project-icon-wrapper">
           {/* <p className="icon-description">visit</p> */}
+
+          {/* <div className="arrow-icon">
+            <a href={url} rel="noopener noreferrer" target="_blank">
+              <svg 
+                  className="icon-link"
+                  xmlns="http://www.w3.org/2000/svg" 
+                  viewBox="0 0 16 16"
+              >
+                  <path fillRule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/>
+              </svg>
+            </a>
+          </div> */}
+
+
             <div className="icons">
               <a href={url} rel="noopener noreferrer" target="_blank">
                   <svg
