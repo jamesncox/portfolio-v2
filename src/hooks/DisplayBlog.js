c75a17075a1578c1be2057428a4b06e4f5bb806a
jamesncox <jamesncox@yahoo.com>
2021-02-23 11:12:11 -0600

    Remove commented-out code from DisplayBlog.js

--- src/hooks/DisplayBlog.js
+++ src/hooks/DisplayBlog.js
@@ -7,7 +7,6 @@ const DisplayBlog = React.memo(({ title, src, placeholder, alt, description, url
       <div className="blog-card">
         <div className="blog-icon-wrapper">
         <p className="blog-title">{title}</p>
-
           <div className="arrow-icon">
             <a href={url} rel="noopener noreferrer" target="_blank">
               <svg 
@@ -18,21 +17,7 @@ const DisplayBlog = React.memo(({ title, src, placeholder, alt, description, url
                   <path fillRule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/>
               </svg>
             </a>
-          </div>
-
-
-            {/* <div className="icons">
-              <a href={url} rel="noopener noreferrer" target="_blank">
-                  <svg
-                    className="icon-link"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 16 16"
-                  >
-                    <path d="M14 0a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h12zM5.904 10.803L10 6.707v2.768a.5.5 0 0 0 1 0V5.5a.5.5 0 0 0-.5-.5H6.525a.5.5 0 1 0 0 1h2.768l-4.096 4.096a.5.5 0 0 0 .707.707z" />
-                  </svg>
-              </a>
-            </div> */}
-            
+          </div>   
         </div>
         <ImageLoad
           className="project-image"
