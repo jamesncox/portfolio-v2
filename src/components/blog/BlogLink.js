f1f45e02ff176268b6a0cbe36b45c618d3f077b4
jamesncox <jamesncox@yahoo.com>
2021-02-23 12:05:53 -0600

    Update BlogLink.js text and add button styling

--- src/components/blog/BlogLink.js
+++ src/components/blog/BlogLink.js
@@ -4,7 +4,7 @@ export default function BlogLink() {
 
     return (
         <a className="see-more-blogs" href="https://dev.to/jamesncox" rel="noopener noreferrer" target="_blank">
-            <p className="text">SEE MORE POSTS</p>
+            <p className="see-more-blogs-text">More Posts</p>
         </a>
     )
 }
