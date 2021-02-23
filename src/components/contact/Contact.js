081c89f4916cf9c2881c82981dbc602e33759ddc
jamesncox <jamesncox@yahoo.com>
2021-02-23 14:20:54 -0600

    Add Responsive.css media queries for .social-grid

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Email from './Email'
 import Resume from './Resume'
+import Social from './Social'
 
 function Contact() {
 
@@ -9,6 +10,7 @@ function Contact() {
       <p className="section-title">contact</p>
       <Email />
       <Resume />
+      <Social />
     </div>
     )
 }
