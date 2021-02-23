bbf3676b4edda42a23d00c09f75876d64651fa1e
jamesncox <jamesncox@yahoo.com>
2021-02-23 10:51:03 -0600

    Update App.css .contact-links and Resume.js <a> inside <p>

--- src/components/contact/Resume.js
+++ src/components/contact/Resume.js
@@ -5,12 +5,16 @@ export default function Resume() {
     return (
         <>
             <p className="section-title">resume</p>
-            <a className="contact-links" href="https://gitconnected.com/jamesncox/resume" rel="noopener noreferrer" target="_blank">
-                <p className="contact-text">@gitconnected</p>
-            </a>
-            <a className="contact-links" href="../../assets/pdfs/James Cox Resume.pdf" download rel="noopener noreferrer" target="_blank">
-                <p className="contact-text">download PDF</p>
-            </a>
+            <p>
+                <a className="contact-links" href="https://gitconnected.com/jamesncox/resume" rel="noopener noreferrer" target="_blank">
+                    @gitconnected
+                </a>
+            </p>
+            <p>
+                <a className="contact-links" href="../../assets/pdfs/James Cox Resume.pdf" download rel="noopener noreferrer" target="_blank">
+                    download PDF
+                </a>
+            </p>
         </>
     )
 }
