d07843f60cc5f960f41324dbf9b45651c26299b5
jamesncox <jamesncox@yahoo.com>
2021-02-23 12:08:16 -0600

    Change .see-more-blog-text to .see-more-text

--- src/components/blog/BlogLink.js
+++ src/components/blog/BlogLink.js
@@ -4,7 +4,7 @@ export default function BlogLink() {
 
     return (
         <a className="see-more-blogs" href="https://dev.to/jamesncox" rel="noopener noreferrer" target="_blank">
-            <p className="see-more-blogs-text">More Posts</p>
+            <p className="see-more-text">More Posts</p>
         </a>
     )
 }
