0eab61171946485e6aac8f145fa2d74236d3968a
jamesncox <jamesncox@yahoo.com>
2021-02-24 10:58:21 -0600

    Add mail symbol to Email.js and .contact-link

--- src/components/contact/Email.js
+++ src/components/contact/Email.js
@@ -3,7 +3,10 @@ import React from 'react'
 export default function Email() {
 
     return (
-        <a className="contact-links" href="mailto:jamesnathanielcox@gmail.com">
+        <a className="email-link" href="mailto:jamesnathanielcox@gmail.com">
+            <svg className="contact-icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
+            </svg>
             jamesnathanielcox@gmail.com
         </a>
     )
--- src/components/contact/Resume.js
+++ src/components/contact/Resume.js
@@ -6,17 +6,17 @@ export default function Resume() {
         <>
             <p className="section-title">resume</p>
             <a className="at-symbol-link" href="https://gitconnected.com/jamesncox/resume" rel="noopener noreferrer" target="_blank">
-                <div className="download-icon">
+                <div className="resume-icon">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207" />
                     </svg>
                 </div>   
-                GITCONNECTED
+                GitConnected
             </a>
             <br />
             <br />
             <a className="pdf-link" href={MyResume} download>
-                <div className="download-icon">
+                <div className="resume-icon">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4" />
                     </svg>
