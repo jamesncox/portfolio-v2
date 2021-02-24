4842f4d7018f510fad0ecf1c26b0eb2545ff9589
jamesncox <jamesncox@yahoo.com>
2021-02-24 09:12:17 -0600

    Add Desktop svg from Heroicons to DisplayProject.js

--- src/hooks/DisplayProject.js
+++ src/hooks/DisplayProject.js
@@ -43,12 +43,8 @@ const DisplayProject = React.memo(({ title, subtitle, src, placeholder, id, alt,
               </div>
               <div className="icons">
                 <a href={url} rel="noopener noreferrer" target="_blank">
-                  <svg
-                    className="icon-link"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 16 16"
-                  >
-                    <path d="M14 0a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h12zM5.904 10.803L10 6.707v2.768a.5.5 0 0 0 1 0V5.5a.5.5 0 0 0-.5-.5H6.525a.5.5 0 1 0 0 1h2.768l-4.096 4.096a.5.5 0 0 0 .707.707z" />
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9.75 17L9 20l-1 1h8l-1-1-.75-3M3 13h18M5 17h14a2 2 0 002-2V5a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
                   </svg>
                 </a>
               </div>
