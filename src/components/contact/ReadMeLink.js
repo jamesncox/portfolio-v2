27fd2befc0d8903c4894ef6325b0d2ca4bc91cc6
jamesncox <jamesncox@yahoo.com>
2021-02-25 12:07:01 -0600

    Update ReadMeLink.js content

--- src/components/contact/ReadMeLink.js
+++ src/components/contact/ReadMeLink.js
@@ -5,7 +5,7 @@ export default function ReadMeLink() {
 
     return (
         <a className="email-link" href="https://github.com/jamesncox/portfolio-v2/blob/main/README.md" rel="noopener noreferrer" target="_blank">
-            Github README.md
+            README.md
         </a>
     )
 }
