ea5adb5e55878887fb0488a22d4c1d104abc77a4
jamesncox <jamesncox@yahoo.com>
2021-02-26 10:37:44 -0600

    Add Joywave Tongues to StalkerBio.js

--- src/components/about/StalkerBio.js
+++ src/components/about/StalkerBio.js
@@ -8,7 +8,10 @@ export default function StalkerBio() {
             <p className="text">Are you stalking me? Do you want to know everything this is to know about me?</p>
             <p className="text">Well you've come to right place.</p>
             <p className="text">Very soon I'll have a lengthy bio up.</p>
-            <p className="text">You might even regret ever reading this much about me.</p>
+            <p className="text">Now please enjoy Joywave's video for their single "Tongues"</p>
+            <div className="image-wrapper">
+                <iframe title="JoyWave Tongues" src="https://www.youtube.com/embed/AqmwKi6KOSw" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+            </div>
         </>
  )
 }
