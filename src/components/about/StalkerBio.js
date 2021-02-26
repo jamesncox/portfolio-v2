06e826fb47ad489df3571f61c149ad08f79dfa6c
jamesncox <jamesncox@yahoo.com>
2021-02-26 12:18:36 -0600

    Update StalkerBio.js

--- src/components/about/StalkerBio.js
+++ src/components/about/StalkerBio.js
@@ -12,7 +12,7 @@ export default function StalkerBio() {
     return (
         <>
             <p className="text">So you want to know EVERYTHING there is to know?</p>
-            <p className="text">I enjoy long walks on the beach.....</p>
+            <p className="text">I enjoy long walks on the beach 🌊.....</p>
             <p className="text">Wait wait wait! This isn't THAT kind of bio. (Even though I actually do enjoy long walks on the beach.)</p>
             <p className="text">Alright, let's get to it.</p>
             <p className="text">Family stuff:</p>
@@ -30,7 +30,7 @@ export default function StalkerBio() {
 
             <p className="text">Hobbies:</p>
             <ul>
-                <li className="text">Music is a big part of my life. I play guitar, drums, piano, sing a little and I listen to music pretty much all day. Except for when I'm coding, strangely not then.</li>
+                <li className="text">Music 🎹 is a big part of my life. I play guitar, drums, piano, sing a little and I listen to music pretty much all day. Except for when I'm coding, strangely not then.</li>
             </ul>
 
             <div className="image-wrapper">
@@ -44,7 +44,7 @@ export default function StalkerBio() {
             <p className="image-text">Proof that I used to rock HARD</p>
                 
             <ul>
-                <li className="text">Soccer is very important to me. My favorite team is Fulham, from London, England, and we ... well ... we do our best! I've been playing soccer since I was 19, and I've gotten pretty good. I think 37-year-old me would give 19-year-old me a run for his money!</li>
+                <li className="text">Soccer ⚽ is very important to me. My favorite team is Fulham, from London, England, and we ... well ... we do our best! I've been playing soccer since I was 19, and I've gotten pretty good. I think 37-year-old me would give 19-year-old me a run for his money!</li>
             </ul>
 
             <div className="image-wrapper">
@@ -55,9 +55,13 @@ export default function StalkerBio() {
                     alt="Dempsey, Konchesky and Etuhu celebrating Dempsey's wonder goal the night they beat Juventus 4-1 at home to advance to the semi-finals"
                 />
             </div>
-            <p className="image-text">Any true Fulham fan will immediately know what night this was and remember THAT goal.</p>
+            <p className="image-text">Any true Fulham fan will immediately know what night this was and remember THAT goal. (YES! Eagle-eyed readers will spot that IS Fulham legend Clint Dempsey, and the inspiration behind our puppy's name.)</p>
 
-            <p className="text">Now please one of my favorite music vidoes, "Tongues" by Joywave.</p>
+            <ul>
+                <li className="text">Fitness 🏋️ is another big part of my life. I run a lot, mainly to stay in shape for soccer. But lately I've started lifting weights more. My wife and recently built a home gym, and we use it almost daily. Having a kid makes you realize your own mortality, and this is my way of delaying the inevitable 😂.</li>
+            </ul>
+
+            <p className="text">Now please one of my favorite music videos, "Tongues" by Joywave.</p>
             <div className="video-wrapper">
                 <iframe className="video" title="JoyWave Tongues" src="https://www.youtube.com/embed/AqmwKi6KOSw" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
             </div>
