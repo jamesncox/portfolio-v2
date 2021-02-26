e58f54632dd895932f34046b2b855c4515e6caf1
jamesncox <jamesncox@yahoo.com>
2021-02-26 11:56:19 -0600

    Add Rock Star.jpg and create .image-text CSS class

--- src/components/about/StalkerBio.js
+++ src/components/about/StalkerBio.js
@@ -4,15 +4,17 @@ import FulhamDempsey from '../../assets/images/Fulham Dempsey.jpg'
 import FulhamDempseyTiny from '../../assets/images/Fulham Dempsey Tiny.jpg'
 import Family from '../../assets/images/Family.jpg'
 import FamilyTiny from '../../assets/images/Family Tiny.jpg'
+import RockStar from '../../assets/images/Rock Star.jpg'
+import RockStarTiny from '../../assets/images/Rock Star Tiny.jpg'
 
 export default function StalkerBio() {
 
     return (
         <>
-            <p className="text">So you want to know <strong>EVERYTHING</strong> there is to know?</p>
+            <p className="text">So you want to know EVERYTHING there is to know?</p>
             <p className="text">I enjoy long walks on the beach.....</p>
             <p className="text">Wait wait wait! This isn't THAT kind of bio. (Even though I actually do enjoy long walks on the beach.)</p>
-            <p className="text">Okay let's get into it.</p>
+            <p className="text">Alright, let's get to it.</p>
             <p className="text">Family stuff:</p>
             <p className="text">I have a beautiful daughter, Ellie, who just turned 8. My wife is a total badass, and we've been together 12 years and married 10 year this July!</p>
 
@@ -24,12 +26,24 @@ export default function StalkerBio() {
                     alt="From left to right: my Rottweiler, Dempsey, my daughter Ellie, my wife, Emily, and myself, James."
                 />
             </div>
-            <p className="text">Not pictured is our Pitt mix, Cocoa. I don't know what she was doing when I snapped this photo.</p>
+            <p className="image-text">(Not pictured is our other pupper, Cocoa. I don't know what she was doing when I snapped this photo.)</p>
 
             <p className="text">Hobbies:</p>
             <ul>
-                <li className="text">Music is a big part of my life. I play guitar, drums, piano, sing a little and I listen to music pretty much every chance I get! Except for when I'm coding, strangely not then.</li>
-                <br />
+                <li className="text">Music is a big part of my life. I play guitar, drums, piano, sing a little and I listen to music pretty much all day. Except for when I'm coding, strangely not then.</li>
+            </ul>
+
+            <div className="image-wrapper">
+                <ImageLoad
+                    className="image"
+                    src={RockStar}
+                    placeholder={RockStarTiny}
+                    alt="From left to right: my Rottweiler, Dempsey, my daughter Ellie, my wife, Emily, and myself, James."
+                />
+            </div>  
+            <p className="image-text">Proof that I used to rock HARD</p>
+                
+            <ul>
                 <li className="text">Soccer is very important to me. My favorite team is Fulham, from London, England, and we ... well ... we do our best!</li>
             </ul>
 
@@ -41,7 +55,7 @@ export default function StalkerBio() {
                     alt="Dempsey, Konchesky and Etuhu celebrating Dempsey's wonder goal the night they beat Juventus 4-1 at home to advance to the semi-finals"
                 />
             </div>
-            <p className="text">Any true Fulham fan will recognize this image and immediately know what night this was and remember THAT goal.</p>
+            <p className="image-text">Any true Fulham fan will immediately know what night this was and remember THAT goal.</p>
 
             <p className="text">Now please one of my favorite music vidoes, "Tongues" by Joywave.</p>
             <div className="video-wrapper">
