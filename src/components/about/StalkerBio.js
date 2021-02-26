1d6d1a67424a47c81cc284ce581d8e5aae57b5b8
jamesncox <jamesncox@yahoo.com>
2021-02-26 10:45:37 -0600

    Update StalkerBio and add .video-wrapper and .video CSS

--- src/components/about/StalkerBio.js
+++ src/components/about/StalkerBio.js
@@ -9,8 +9,8 @@ export default function StalkerBio() {
             <p className="text">Well you've come to right place.</p>
             <p className="text">Very soon I'll have a lengthy bio up.</p>
             <p className="text">Now please enjoy Joywave's video for their single "Tongues"</p>
-            <div className="image-wrapper">
-                <iframe title="JoyWave Tongues" src="https://www.youtube.com/embed/AqmwKi6KOSw" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+            <div className="video-wrapper">
+                <iframe className="video" title="JoyWave Tongues" src="https://www.youtube.com/embed/AqmwKi6KOSw" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
             </div>
         </>
  )
