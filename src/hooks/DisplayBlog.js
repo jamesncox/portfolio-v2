ced4352afa93daa9944691a4e264693d04d6747f
jamesncox <jamesncox@yahoo.com>
2021-02-28 17:46:13 -0600

    Update DisplayBlog.js with SVG <title> and <desc>

--- src/hooks/DisplayBlog.js
+++ src/hooks/DisplayBlog.js
@@ -9,7 +9,9 @@ const DisplayBlog = React.memo(({ title, src, placeholder, alt, description, url
         <p className="blog-title">{title}</p>
           <div className="arrow-icon">
             <a href={url} rel="noopener noreferrer" target="_blank">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" aria-labelledby="title">
+              <title id="title" lang="en">Arrow icon</title>
+              <desc>Click the arrow icon to visit {title} on Dev.to</desc>
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M13 9l3 3m0 0l-3 3m3-3H8m13 0a9 9 0 11-18 0 9 9 0 0118 0z" />
               </svg>
             </a>
