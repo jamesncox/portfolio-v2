b292e104903b424b35582a354a9ceb054d01eb64
jamesncox <jamesncox@yahoo.com>
2021-02-28 17:10:47 -0600

    Change Work.js <p> to <h2>

--- src/components/about/Work.js
+++ src/components/about/Work.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Work() {
     return(
         <>
-            <p className="section-title">work</p>
+            <h2 className="section-title">work</h2>
             <p className="text">
                 Currently I am freelancing, and accepting new clients. But if you
                 believe I would be a good fit for a role, I am happy to hear you out.
