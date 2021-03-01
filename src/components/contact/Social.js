01ed6d85a968f7ae4ab739a00e4d19002db24db5
jamesncox <jamesncox@yahoo.com>
2021-03-01 09:01:24 -0600

    Add aria-labelledby to Social.js Github

--- src/components/contact/Social.js
+++ src/components/contact/Social.js
@@ -28,7 +28,10 @@ export default function Social() {
                         <svg 
                             viewBox="0 0 128 128"
                             width="60"
+                            aria-labelledby="github-social"
                         >
+                        <title id="github-social" lang="en">Github icon</title>
+                        <desc>Link to James Cox's Github profile</desc>
                         <path
                             className="github-icon"
                             fillRule="evenodd"
