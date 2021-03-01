5beb72268f3d1a5f2ccbc1f3b06ba2103032c6bc
jamesncox <jamesncox@yahoo.com>
2021-03-01 09:51:11 -0600

    Update DisplayBlog <desc> text

--- src/hooks/DisplayBlog.js
+++ src/hooks/DisplayBlog.js
@@ -11,7 +11,7 @@ const DisplayBlog = React.memo(({ title, src, placeholder, alt, description, url
             <a href={url} rel="noopener noreferrer" target="_blank">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" aria-labelledby="arrow-title arrow-desc">
               <title id="arrow-title" lang="en">Arrow icon</title>
-              <desc id="arrow-desc">Click the arrow icon to visit blog Dev.to</desc>
+              <desc id="arrow-desc">Visit {title} blog at Dev.to</desc>
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M13 9l3 3m0 0l-3 3m3-3H8m13 0a9 9 0 11-18 0 9 9 0 0118 0z" />
               </svg>
             </a>
