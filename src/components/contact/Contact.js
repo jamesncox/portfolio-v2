090b5d15b8896620c43cb9fd0f50504c26bfb9e0
jamesncox <jamesncox@yahoo.com>
2021-03-25 09:34:56 -0500

    Remove Import React... from all contact components

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,23 +1,21 @@
-import React from 'react'
-import Email from './Email'
-import Resume from './Resume'
-import Social from './Social'
-import ReadMeLink from './ReadMeLink'
+import Email from "./Email";
+import Resume from "./Resume";
+import Social from "./Social";
+import ReadMeLink from "./ReadMeLink";
 
 function Contact() {
-
-    return (
-      <div  id="contact">
-        <h2 className="section-title">contact</h2>
-        <Email />
-        <h2 className="section-title">resume</h2>
-        <Resume />
-        <h2 className="section-title">portfolio details</h2>
-        <ReadMeLink />
-        <h2 className="section-title">social</h2>
-        <Social />
-      </div>
-    )
+  return (
+    <div id="contact">
+      <h2 className="section-title">contact</h2>
+      <Email />
+      <h2 className="section-title">resume</h2>
+      <Resume />
+      <h2 className="section-title">portfolio details</h2>
+      <ReadMeLink />
+      <h2 className="section-title">social</h2>
+      <Social />
+    </div>
+  );
 }
 
-export default Contact
+export default Contact;
--- src/components/contact/Email.js
+++ src/components/contact/Email.js
@@ -1,15 +1,16 @@
-import React from 'react'
-
 export default function Email() {
-
-    return (
-        <a className="email-link" href="mailto:jamesnathanielcox@gmail.com">
-            <div className="email-icon">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
-                </svg>
-            </div>
-            jamesnathanielcox@gmail.com
-        </a>
-    )
+  return (
+    <a className="email-link" href="mailto:jamesnathanielcox@gmail.com">
+      <div className="email-icon">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
+          />
+        </svg>
+      </div>
+      jamesnathanielcox@gmail.com
+    </a>
+  );
 }
