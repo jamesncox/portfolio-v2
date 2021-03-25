51f52ea2dacd0374147c596e8112f196e0e79ca7
jamesncox <jamesncox@yahoo.com>
2021-03-25 09:07:52 -0500

    Update BioSelect.js with lengthOptions as object and two maps

--- src/components/about/BioSelect.js
+++ src/components/about/BioSelect.js
@@ -1,13 +1,18 @@
 import React, { useState } from "react";
-import ShortBio from "./ShortBio";
-import MediumBio from "./MediumBio";
 import LongBio from "./LongBio";
+import MediumBio from "./MediumBio";
+import ShortBio from "./ShortBio";
 import StalkerBio from "./StalkerBio";
 
-const lengthOptions = ["short", "medium", "long", "stalker"];
+const lengthOptions = [
+  { type: "short", Component: ShortBio },
+  { type: "medium", Component: MediumBio },
+  { type: "long", Component: LongBio },
+  { type: "stalker", Component: StalkerBio },
+];
 
 function BioSelect() {
-  const [selectedLength, setSelectedLength] = useState(lengthOptions[0]);
+  const [selectedLength, setSelectedLength] = useState("short");
 
   const toggleLength = (e) => {
     setSelectedLength(e.target.id);
@@ -16,64 +21,21 @@ function BioSelect() {
   return (
     <>
       <nav className="bio-menu-wrapper">
-        {lengthOptions.map((length) => (
+        {lengthOptions.map(({ type, Component }) => (
           <h2
-            key={length}
-            id={length}
-            className={length === selectedLength ? "selected-bio" : "bio-menu"}
+            key={type}
+            id={type}
+            className={type === selectedLength ? "selected-bio" : "bio-menu"}
             onClick={toggleLength}
           >
-            {length}
+            {type}
           </h2>
         ))}
       </nav>
-      {selectedLength === "short" && <ShortBio />}
-      {selectedLength === "medium" && <MediumBio />}
-      {selectedLength === "long" && <LongBio />}
-      {selectedLength === "stalker" && <StalkerBio />}
+      {lengthOptions.map(
+        ({ type, Component }) => selectedLength === type && <Component />
+      )}
     </>
   );
 }
 export default BioSelect;
-
-// import React, { useState } from "react";
-// import LongBio from "./LongBio";
-// import MediumBio from "./MediumBio";
-// import ShortBio from "./ShortBio";
-// import StalkerBio from "./StalkerBio";
-
-// const lengthOptions = [
-//   { type: "short", Component: ShortBio },
-//   { type: "medium", Component: MediumBio },
-//   { type: "long", Component: LongBio },
-//   { type: "stalker", Component: StalkerBio },
-// ];
-
-// function BioSelect() {
-//   const [selectedLength, setSelectedLength] = useState("short");
-
-//   const toggleLength = (e) => {
-//     setSelectedLength(e.target.id);
-//   };
-
-//   return (
-//     <>
-//       {lengthOptions.map(({ type, Component }) => (
-//         <>
-//           <nav className="bio-menu-wrapper">
-//             <h2
-//               key={type}
-//               id={type}
-//               className={type === selectedLength ? "selected-bio" : "bio-menu"}
-//               onClick={toggleLength}
-//             >
-//               {type}
-//             </h2>
-//           </nav>
-//           {selectedLength === type && <Component />}
-//         </>
-//       ))}
-//     </>
-//   );
-// }
-// export default BioSelect;
