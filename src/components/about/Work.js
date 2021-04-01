4935b76ca853d194f328184a5a0bc1e3a9b06d73
jamesncox <jamesncox@yahoo.com>
2021-04-01 14:55:27 -0500

    Update Work.js content

--- src/components/about/Work.js
+++ src/components/about/Work.js
@@ -4,7 +4,9 @@ export default function Work() {
       <h2 className="section-title">work</h2>
       <p className="text">
         Currently I am freelancing, and accepting new clients. But if you
-        believe I would be a good fit for a role, I am happy to hear you out. I
+        believe I would be a good fit for a role, I am happy to discuss the
+        opportunity. I try to live my life with integrity and honesty, and if I
+        don't believe something is a good fit, I promise to tell the truth. I
         will always give honest feedback, scout's honor.
       </p>
     </>
