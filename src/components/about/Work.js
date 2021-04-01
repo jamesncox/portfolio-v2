a16835b26637e5a8f5d0d209ef23246e4784df41
jamesncox <jamesncox@yahoo.com>
2021-04-01 14:57:49 -0500

    Update Work.js content add paragraph

--- src/components/about/Work.js
+++ src/components/about/Work.js
@@ -5,9 +5,12 @@ export default function Work() {
       <p className="text">
         Currently I am freelancing, and accepting new clients. But if you
         believe I would be a good fit for a role, I am happy to discuss the
-        opportunity. I try to live my life with integrity and honesty, and if I
-        don't believe something is a good fit, I promise to tell the truth. I
-        will always give honest feedback, scout's honor.
+        opportunity.
+      </p>
+      <p className="text">
+        I try to live my life with integrity and honesty, and if I don't believe
+        something is a good fit, I promise to tell the truth. I will always give
+        honest feedback, scout's honor.
       </p>
     </>
   );
