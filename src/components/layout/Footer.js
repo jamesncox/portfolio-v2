4484e5a415737801e70210143b7f7e5a7562b3b1
jamesncox <jamesncox@yahoo.com>
2021-05-03 10:09:20 -0500

    Create footer link to twitter and add styles

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,7 +1,17 @@
 export default function Footer() {
   return (
     <div className="footer">
-      <p>Made with ❤️‍ by James Cox</p>
+      <p>
+        Made with ❤️‍ by{" "}
+        <a
+          className="footer-link"
+          href="https://twitter.com/MemRook"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          James Cox
+        </a>
+      </p>
       <p>
         {"Copyright © "}
         {new Date().getFullYear()}
