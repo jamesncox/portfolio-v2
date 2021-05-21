435db225d2ce1ad90410a8ddf809167df2df2379
jamesncox <jamesncox@yahoo.com>
2021-05-21 14:48:42 -0500

    Update Work section to remove freelance tidbit

--- src/components/about/Work.js
+++ src/components/about/Work.js
@@ -3,14 +3,14 @@ export default function Work() {
     <>
       <h2 className="section-title">work</h2>
       <p className="text">
-        Currently I am freelancing, and accepting new clients. But if you
-        believe I would be a good fit for a role, I am happy to discuss the
-        opportunity.
+        I am a former restaurant manager turned stay-at-home father looking for
+        my first developer role. I am open to all new opportunities. However, in
+        5 to 10 years I see myself working and thriving in product development.
       </p>
       <p className="text">
-        I try to live my life with integrity and honesty, and if I don't believe
-        something is a good fit, I promise to tell the truth. I will always give
-        honest feedback, scout's honor.
+        If you believe I would be a good fit for your team, please do not
+        hesitate to reach out and I would love to chat with you. Thank you so
+        much for checking out my portfolio!
       </p>
     </>
   );
